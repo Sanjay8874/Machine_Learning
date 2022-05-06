abb926a5522c8a28c20abd55f29d87f294b5fc60
Sanjay <sanjay8874002308@gmail.com>
2022-05-06 16:10:57 +0530

    Start data preprocessing

---
+++ Data Preprocessing/Data_Preprocessing.py
@@ -0,0 +1,9 @@
+#data preprocessing
+
+#import libraries
+import numpy as np # use to mathemetical
+import pandas as pd # use to dataSet
+import matplotlib.pyplot as plt # use to plot gragph
+
+#import dataset
+dataset = pd.read_csv("Data.csv")
