f3aef8071c8574d23d97b817105e5f17cbf102f4
Sanjay <sanjay8874002308@gmail.com>
2022-05-06 16:12:59 +0530

    dependent variable and indeendent variable

--- Data Preprocessing/Data_Preprocessing.py
+++ Data Preprocessing/Data_Preprocessing.py
@@ -7,3 +7,7 @@ import matplotlib.pyplot as plt # use to plot gragph
 
 #import dataset
 dataset = pd.read_csv("Data.csv")
+
+#Two variable dependent(Y) and independent(X)
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,3].values
