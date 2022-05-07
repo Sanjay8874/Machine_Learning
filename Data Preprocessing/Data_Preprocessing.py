31c881ac457b9dea784d5490c9202f8f9e2e2d51
Sanjay <sanjay8874002308@gmail.com>
2022-05-07 17:22:28 +0530

    Taking care of missing data(code for manage missing data)

--- Data Preprocessing/Data_Preprocessing.py
+++ Data Preprocessing/Data_Preprocessing.py
@@ -11,3 +11,11 @@ dataset = pd.read_csv("Data.csv")
 #Two variable dependent(Y) and independent(X)
 X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,3].values
+
+
+#Taking care of missing dataset
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.NaN, strategy = "mean")
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:,1:3])
+
