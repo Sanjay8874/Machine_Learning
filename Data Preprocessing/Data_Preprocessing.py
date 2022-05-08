79de8594f9b371c66129e34bc338eb89a4872036
Sanjay <sanjay8874002308@gmail.com>
2022-05-08 16:13:01 +0530

    Encoding categorical data

--- Data Preprocessing/Data_Preprocessing.py
+++ Data Preprocessing/Data_Preprocessing.py
@@ -19,3 +19,40 @@ imputer = SimpleImputer(missing_values = np.NaN, strategy = "mean")
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:,1:3])
 
+
+#Encoding categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X= LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+#onehotencoder = OneHotEncoder(categories = [0])
+#X = onehotencoder.fit_transform(X).toarray() # In the new version this line not execute properly.
+#X = pd.get_dummies(X[:,0])
+
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(categories='auto'), [0])],
+    remainder='passthrough')
+X = ct.fit_transform(X)
+
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
